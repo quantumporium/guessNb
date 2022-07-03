b1f2221c5a4762b496a1c56f6e1e63a61befe327
quantumporium <quantum@email.com>
2022-07-02 22:39:56 -0700

    first commit, not much left to do

---
+++ README.md
@@ -0,0 +1,28 @@
+# guess the random number (guessNb)
+Guess the random number (guessNb) is a terminal game in which you have to guess the randomnumber - the name is more or less self explanatory.
+
+## Table of content
+- [requirement](#requirement)
+- [usage](#usage)
+- [license](#license)
+
+## Requirement
+You need node.js and prompt-sync for this Javascrypt code to work.
+
+Use [this](https://nodeJs.org/en/) if you don't have nodeJs already installed.
+
+Then use [npm](https://docs.npmjs.com/dowloading-and-installing-node-js-and-npm) to install prompt.
+
+To install prompt use this command: ````sh npm install prompt-sync````
+
+## Usage
+In order to use guessNb you just need to fired it up. 
+Either use the dot syntax or NodeJs.
+
+```sh
+./guessNb.js	# to use dot syntax
+node guessNb.js # the nodeJs
+```
+
+## License
+This scrypt use the [GPLv2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) license.
---
+++ script/guessNb.js
@@ -0,0 +1,76 @@
+#!/usr/bin/node
+/*
+ * name = guess the number (guessNb)
+ * author = quantumporium
+ * email  = quantum[at]porium.ca / this email doesn't exist yet??
+ * goal	  = terminal based guess the number game made with javascrypt and nodeJs
+ *
+ */
+
+/*
+ * functionality and usage
+ * [ example usage ]
+ * guess the mistery number ___ .
+ *  Your guess 21
+ * your guess is to high, try again .
+ *  your guess 10
+ * your guess it to low, try again .
+ *  your guess a
+ * your guess is not valid .
+ *  your guess 15
+ * correct the mistery number was 15 .
+ * [ /example usage ]
+ *
+ * [ function and functionality ]
+ * createRdnumber() -> int randomNb (pure function)
+ * validateInput() -> string as return / some things
+ * main() -> user interaction is made here (non pure function)
+ * [ /function and functionality ]
+ *
+ */
+
+const prompt = require("prompt-sync")();	// install prompt-sync library
+
+
+function createRdNumber(Max) {
+	return Math.random() * Max
+}
+
+function parseInput(userInput, rdNumber) {
+	if( userInput == rdNumber ){
+		console.log(" correct the mistery number was "+rdNumber+".");
+		process.exit()	// exit when right answer	
+	}
+	else if( userInput < rdNumber){
+		console.log(" you guess it to low. ");
+		
+	}
+	else if( userInput > rdNumber){
+		console.log(" your guess is to high. ");
+		
+	}
+	else{
+		console.log(" [ ERROR ] a fatal error happen ");
+		
+	}
+}
+
+function main() {
+	let Max = 100	// You can change this value
+	console.log("***** Welcome to guessNb *****");
+	console.log(` [ RULE ] You have to guess a number between 0 and ${Max}`);
+	const rdNumber = Math.round( createRdNumber(Max) );
+	
+	for(let i = 10; i >= 0; i-= 1){
+		let userInput = prompt(" Enter your guess: ");
+		parseInput(userInput, rdNumber);
+
+		console.log(` You have ${i} guess left.`);
+	}
+	
+	console.log(" Your guess ran out ...");
+	console.log(" The correct answer was "+rdNumber);
+	process.exit();
+}
+
+main()
